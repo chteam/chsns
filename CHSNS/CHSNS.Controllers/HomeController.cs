58e4ef65fd0dcb9f956c63fbd48c36d15eba140c
chsword_cp <SND\chsword_cp>
2008-07-04 16:01:20 +0000

    换做ASP.netMVC的第一步

---
+++ CHSNS/CHSNS.Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CHSNS.Controllers {
+	public class HomeController : Controller {
+		public ActionResult Index() {
+			// Add action logic here
+			return View();
+		}
+	}
+}
---
+++ CHSNS/CHSNS/System.Web.Mvc/HtmlHelper/Include.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CHSNS;
+
+namespace System.Web.Mvc {
+	static public class Include {
+		static public string Script(this HtmlHelper h,String fn){
+			return
+				string.Format("<script type=\"text/javascript\" src=\"{0}Js/{1}.js\"></script>"
+				, SiteConfig.Current.Path, fn.TrimEnd(".js".ToCharArray()));
+		}
+		static public string CSSLink(this HtmlHelper h, String fn) {
+			return	string.Format(
+				"<link href=\"{0}Style/{1}/master.css\" rel=\"stylesheet\" type=\"text/css\" />"
+				, SiteConfig.Current.Path
+				,SiteConfig.Current.Style
+				, fn.TrimEnd(".css".ToCharArray())
+				);
+		}
+	}
+}
