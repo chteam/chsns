10c17e97151fb3915224659f074f0c8528ef8292
chsword_cp <SND\chsword_cp>
2008-07-06 02:58:01 +0000

    用户静态类

---
+++ CHSNS/CHSNS/CHUser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web;
+
+namespace CHSNS {
+	public class CHUser {
+		static public string NickName {
+			get {
+				if (String.IsNullOrEmpty(HttpContext.Current.Response.Cookies["CHSNS"]["nn"]))
+					return HttpContext.Current.Response.Cookies["CHSNS"]["nn"];
+				return String.Empty;
+			}
+		}
+		static public long UserID {
+			get {
+				Int64 userid = 0;
+				if (String.IsNullOrEmpty(HttpContext.Current.Response.Cookies["CHSNS"]["id"])) {
+					Int64.TryParse(HttpContext.Current.Response.Cookies["CHSNS"]["id"], out userid);
+				}
+				return userid;
+			}
+		}
+
+		static public Boolean IsLogin {
+			get {
+				return CHUser.UserID != 0;
+			}
+		}
+	}
+}
