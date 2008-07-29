f1435247605f9c4529f69f175a3ad4e44f186beb
chsword_cp <SND\chsword_cp>
2008-07-29 15:51:33 +0000

    Dic的添加

---
+++ CHSNS/CHSNS/Dictionary.cs
@@ -0,0 +1,15 @@
+﻿namespace CHSNS {
+	using System.Collections.Generic;
+	using System;
+	public class Dictionary : Dictionary<string ,object> {
+		public static Dictionary CreateFromArgs(params object[] args) {
+			if (args.Length % 2 != 0)
+				throw new Exception("不可以有奇数个传入数据");
+			Dictionary dict = new Dictionary();
+			for (int i = 0; i < args.Length; i += 2) {
+				dict.Add(args[i].ToString(), args[i + 1]);
+			}
+			return dict;
+		}
+	}
+}
