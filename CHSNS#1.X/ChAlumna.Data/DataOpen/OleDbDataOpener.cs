8b9830015ff6798f2560d5456b5eaf1a9c5fa12d
chsword_cp <SND\chsword_cp>
2008-08-20 15:20:47 +0000

    DataOpener

---
+++ CHSNS#1.X/ChAlumna.Data/DataOpen/OleDbDataOpener.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Text;
+using System.Data.OleDb;
+using System.Data.Common;
+using System.Data;
+
+namespace CHSNS {
+	public class OleDbDataOpener : IDataOpener {
+		OleDbConnection _Connection;
+		OleDbCommand _Command;
+
+		public DbCommand Command {
+			get { return _Command; }
+		}
+		public OleDbDataOpener(string ConnectionString) {
+			_Connection = new OleDbConnection(ConnectionString);
+			_Command = new OleDbCommand();
+		}
+		#region 打开关闭数据库
+		void Open(CommandType type, string text) {
+			this.Command.Connection = _Connection;
+			this.Command.CommandType = type;
+			this.Command.CommandText = text;
+			if (_Connection.State != ConnectionState.Open)
+				this.Command.Connection.Open();
+		}
+		void  IDataOpener.Open(string SQLtext) {
+			Open(CommandType.Text, SQLtext);
+		}
+
+		public void Close() {
+			this.Command.Parameters.Clear();
+			
+		}
+		public DbDataAdapter GetAdapter() {
+			//throw new Exception(Command.CommandText);
+			return new OleDbDataAdapter(_Command);
+		}
+		public void AddWithValue(string key, object value) {
+			_Command.Parameters.AddWithValue(key, value);
+		}
+		public void Dispose() {
+			if (_Connection.State != ConnectionState.Closed)
+				Command.Connection.Close();
+			Command.Dispose();
+			_Connection.Dispose();
+		}
+
+		#endregion
+	}
+}
---
+++ CHSNS#1.X/ChAlumna.Data/DataOpen/SqlStoredProcedureDataOpener.cs
@@ -0,0 +1,13 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace CHSNS {
+
+	public class SqlStoredProcedureDataOpener : SqlDataOpener {
+		public SqlStoredProcedureDataOpener(string ConnectionString)
+			: base(ConnectionString) {
+			this.CommandType = CommandType.StoredProcedure;
+		}
+	}
+}
