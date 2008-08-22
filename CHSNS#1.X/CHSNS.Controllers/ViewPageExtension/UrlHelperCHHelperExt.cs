49c43dc3e636c3a14a95346a2d81454b8856633b
chsword_cp <SND\chsword_cp>
2008-08-22 14:35:48 +0000

    Add a CH helper

---
+++ CHSNS#1.X/CHSNS.Controllers/ViewPageExtension/UrlHelperCHHelperExt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace CHSNS  {
+	public static class UrlHelperCHHelperExt {
+		public static ChHelper CH(this UrlHelper vc) {
+			return new ChHelper();
+		}
+	}
+}
