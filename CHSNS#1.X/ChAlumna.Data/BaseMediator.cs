833b1717932137aafae6b94ce2a17a1b52a33c46
chsword_cp <SND\chsword_cp>
2008-08-30 11:09:22 +0000

    清理中介者

--- CHSNS#1.X/ChAlumna.Data/BaseMediator.cs
+++ CHSNS#1.X/ChAlumna.Data/BaseMediator.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace CHSNS.Data {
+﻿namespace CHSNS.Data {
 	public class BaseMediator : IMediator {
 
 		#region IMediator 成员
