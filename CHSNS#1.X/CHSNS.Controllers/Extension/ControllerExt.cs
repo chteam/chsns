a35c4bc3945580accfd9e94b7ac81c1afff4488f
chsword_cp <SND\chsword_cp>
2008-08-30 11:23:37 +0000

    Controller的扩展

---
+++ CHSNS#1.X/CHSNS.Controllers/Extension/ControllerExt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace CHSNS.Extension
+{
+	public static class ControllerExt {
+		public static string QueryString(this Controller c,string QueryStringName)
+		{
+			return string.IsNullOrEmpty(c.Request.QueryString[QueryStringName]) ? string.Empty : c.Request.QueryString[QueryStringName].ToLower();
+		}
+
+		public static int QueryNum(this Controller c, string QueryStringName)
+		{
+			return string.IsNullOrEmpty(c.QueryString(QueryStringName)) ? 0 : Convert.ToInt32(c.QueryString(QueryStringName));
+		}
+
+		public static long QueryLong(this Controller c, string QueryStringName)
+		{
+			return string.IsNullOrEmpty(c.QueryString(QueryStringName)) ? 0 : Convert.ToInt64(c.QueryString(QueryStringName));
+		}
+	}
+}
---
+++ CHSNS#1.X/CHSNS.Controllers/Extension/RedirectExt.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace CHSNS.Extension
+{
+	/// <summary>
+	/// 对Controller的Redirect操作的扩展
+	/// blog:http://chsword.cnblogs.com/
+	/// </summary>
+	public static class RedirectExtension {
+		/// <summary>
+		/// 重定向到上一个Action. 即 header 的 "HTTP_REFERER"  (<c>Context.UrlReferrer</c>).
+		/// </summary>
+		static public RedirectResult RedirectToReferrer(this Controller controller) {
+			return new RedirectResult(
+				controller.Request.ServerVariables["HTTP_REFERER"]
+				);
+		}
+		/// <summary> 
+		/// Redirect 到站点根目录 (<c>Context.ApplicationPath + "/"</c>).
+		/// </summary>
+		static public RedirectResult RedirectToSiteRoot(this Controller controller) {
+			return new RedirectResult(controller.Request.ApplicationPath + "/");
+		}
+
+	}
+}
