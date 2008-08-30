29057daefb4fdcfd480de2e3d2f30583b565f7d8
chsword_cp <SND\chsword_cp>
2008-08-30 11:00:56 +0000

    已经全部升级至Pv5，更Filter

---
+++ CHSNS#1.X/CHSNS.Controllers/Filter/OnlineFilter.cs
@@ -0,0 +1,33 @@
+using CHSNS;
+using System.Web.Mvc;
+
+namespace CHSNS.Filter
+{
+	public class OnlineFilter : ActionFilterAttribute {
+		public override void OnActionExecuting(ActionExecutingContext filterContext) {            
+			//if (CHCache.IsNullorEmpty("Application.Showpage")) {
+			//    if (!System.IO.File.Exists(ChServer.MapPath("/Chsword.lic"))) {
+			//        System.IO.File.Create(ChServer.MapPath("/Chsword.lic"));
+			//        CHCache.SetCache("Application.Showpage", false);
+			//    } else {
+			//        NetworkCross net = new NetworkCross();
+			//        string mac = net.GetNetCardMacAddress();
+			//        Encrypt en = new Encrypt();
+			//        string regwill = en.MD5Encrypt(en.DESEncrypt(mac, "40717407"), 32);
+			//        string regstr = File.ReadAllText(ChServer.MapPath("/Chsword.lic"));
+			//        if (regwill == regstr)
+			//            CHCache.SetCache("Application.Showpage", true);
+			//        else
+			//            CHCache.SetCache("Application.Showpage", false);
+			//    }
+			//}
+			//if (!Convert.ToBoolean(CHCache.GetCache("Application.Showpage"))) {
+			//    context.Response.Write("您的应用程序没有注册请联系CHSNS工作人员进行注册" +
+			//        "<a href='http://www.eice.com.cn'>www.eice.com.cn</a>");
+			//    return false;
+			//}
+			Online.Update();
+			Online.RemoveOffline();
+		}
+	}
+}
