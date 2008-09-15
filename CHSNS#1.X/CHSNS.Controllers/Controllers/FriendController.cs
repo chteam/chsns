c419e90e67e4b134fc73fe4d48f05c47b7db8d83
chsword_cp <SND\chsword_cp>
2008-09-15 17:23:47 +0000

    Friend全事务

--- CHSNS#1.X/CHSNS.Controllers/Controllers/FriendController.cs
+++ CHSNS#1.X/CHSNS.Controllers/Controllers/FriendController.cs
@@ -72,10 +72,15 @@ namespace CHSNS.Controllers {
 		#region ajax execute
 		[LoginedFilter]
 		[AcceptVerbs("Post")]
-		public ActionResult Add(long toid) {//添加好友
-			if (DBExt.Friend.Add(CHUser.UserID, toid))
-				return Content("已经向对方发出请求");
-			return Content("对方已经是你的好友");
+		public ActionResult Add(long toid)
+		{//添加好友
+			using (var ts = new TransactionScope())
+			{
+				var x = DBExt.Friend.Add(CHUser.UserID, toid);
+				ts.Complete();
+				if (x) return Content("已经向对方发出请求");
+				return Content("对方已经是你的好友");
+			}
 		}
 		/// <summary>
 		/// 删除
@@ -85,28 +90,45 @@ namespace CHSNS.Controllers {
 		[LoginedFilter]
 		[AcceptVerbs("Post")]
 		public ActionResult Delete(long toid) {
-			DBExt.Friend.Delete(CHUser.UserID, toid);
-			return Content("解除关系成功");
+			using (var ts = new TransactionScope())
+			{
+				DBExt.Friend.Delete(CHUser.UserID, toid);
+				ts.Complete();
+				return Content("解除关系成功");
+			}
 		}
 		[LoginedFilter]
 		[AcceptVerbs("Post")]
-		public ActionResult Agree(long uid) {//添加好友
-			if (DBExt.Friend.Agree(CHUser.UserID, uid))
-				return Content("已经加对方为好友");
-			return Content("请求已经处理过了");
+		public ActionResult Agree(long uid)
+		{//添加好友
+			using (var ts = new TransactionScope())
+			{
+				var r = DBExt.Friend.Agree(CHUser.UserID, uid);
+				ts.Complete();
+				if (r) return Content("已经加对方为好友");
+				return Content("请求已经处理过了");
+			}
 		}
 		[LoginedFilter]
 		[AcceptVerbs("Post")]
 		public ActionResult Ignore(long uid) {//添加好友
-			if (DBExt.Friend.Ignore(uid, CHUser.UserID))
-				return Content("已经忽略了请求");
-			return Content("请求已经处理过了");
+			using (var ts = new TransactionScope())
+			{
+				var r=DBExt.Friend.Ignore(uid, CHUser.UserID);
+				ts.Complete();
+				if (r)	return Content("已经忽略了请求");
+				return Content("请求已经处理过了");
+			}
 		}
 		[LoginedFilter]
 		[AcceptVerbs("Post")]
 		public ActionResult IgnoreAll() {//添加好友
-			DBExt.Friend.IgnoreAll(CHUser.UserID);
-			return Content("已经忽略所有请求");
+			using (var ts = new TransactionScope())
+			{
+				DBExt.Friend.IgnoreAll(CHUser.UserID);
+				ts.Complete();
+				return Content("已经忽略所有请求");
+			}
 		}
 		#endregion
 	}
