de5ac7a94c0eb9ddcd19e01e7c6d95142c3fc48c
chsword_cp <SND\chsword_cp>
2008-10-03 15:52:19 +0000

    完成

---
+++ CHSNS#1.X/CHSNS.Core/Email/IEmail.cs
@@ -0,0 +1,6 @@
+﻿using System;
+namespace CHSNS.Email {
+	public interface IEmail {
+		void Send(string to, string from, string subject, string body, string username, string password, string smtpHost);
+	}
+}
