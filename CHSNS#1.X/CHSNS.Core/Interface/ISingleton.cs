d72968a30f13ba7b1f435cb1eea4d2a088f406b5
chsword_cp <SND\chsword_cp>
2008-10-10 02:09:26 +0000

    the lastest checkin and I'll go to Biejing

---
+++ CHSNS#1.X/CHSNS.Core/Interface/ISingleton.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CHSNS {
+	public interface ISingleton<T>  {
+		string SingletonString { get; }
+		T Value { get; set; }
+	}
+}
