6cb1a83b8769b6e2d8ac74107078682ccb4c26d0
chsword_cp <SND\chsword_cp>
2008-11-06 12:36:11 +0000

    转为asp.net mvc beta

--- CHSNS#1.X/CHSNS.Data/GroupMediator.cs
+++ CHSNS#1.X/CHSNS.Data/GroupMediator.cs
@@ -18,5 +18,46 @@ namespace CHSNS.Data {
 				"@GroupClass", 0);
 		}
 
+
+		#region IGroupMediator 成员
+
+
+		public Group Info(long ID) {
+			throw new System.NotImplementedException();
+		}
+
+		public bool Add(Group group) {
+			throw new System.NotImplementedException();
+		}
+
+		public bool Delete(long ID) {
+			throw new System.NotImplementedException();
+		}
+
+		public bool Update(Group group) {
+			throw new System.NotImplementedException();
+		}
+
+		public System.Linq.IQueryable<CHSNS.ModelPas.NotePas> NoteList(long ID) {
+			throw new System.NotImplementedException();
+		}
+
+		public bool Join(GroupUser guser) {
+			throw new System.NotImplementedException();
+		}
+
+		public bool Level(GroupUser guser) {
+			throw new System.NotImplementedException();
+		}
+
+		public bool ToAdmin(GroupUser guser, long operaterID) {
+			throw new System.NotImplementedException();
+		}
+
+		public bool ToCommonUser(GroupUser guser, long operaterID) {
+			throw new System.NotImplementedException();
+		}
+
+		#endregion
 	}
 }
