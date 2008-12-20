724b8a795beb0329b1eccea4eb0670cf53482316
chsword_cp <SND\chsword_cp>
2008-12-20 14:58:41 +0000

    db wiki

---
+++ CHSNS#1.X/CHSNS.Database/Schema Objects/Tables/dbo.Entry.table.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[Entry](
+	[ID] [bigint] IDENTITY(10000,1) NOT NULL,
+	[Title] [nvarchar](50) NOT NULL,
+	[CreaterID] [bigint] NOT NULL,
+	[UpdateTime] [smalldatetime] NOT NULL CONSTRAINT [DF_Entry_UpdateTime]  DEFAULT (getdate()),
+	[CurrentID] [bigint] NULL,
+	[EditCount] [int] NOT NULL,
+	[ViewCount] [bigint] NOT NULL,
+	[Status] [int] NOT NULL,
+	[Ext] [ntext] NULL,
+CONSTRAINT [PK_Entry] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX  = OFF, 
+	STATISTICS_NORECOMPUTE  = OFF, 
+	IGNORE_DUP_KEY = OFF,
+	ALLOW_ROW_LOCKS  = ON, 
+	ALLOW_PAGE_LOCKS  = ON
+	) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
---
+++ CHSNS#1.X/CHSNS.Database/Schema Objects/Tables/dbo.EntryVersion.table.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [dbo].[EntryVersion](
+	[ID] [bigint] IDENTITY(1,1) NOT NULL,
+	[Reason] [text] NOT NULL,
+	[AddTime] [smalldatetime] NOT NULL,
+	[Description] [text] NOT NULL,
+	[Reference] [text] NOT NULL,
+	[UserID] [bigint] NOT NULL,
+	[Status] [int] NOT NULL CONSTRAINT [DF_EntryVersion_Status]  DEFAULT ((0)),
+	[EntryID] [bigint] NULL,
+	[AreaID] [int] NOT NULL,
+	[Ext] [text] NULL,
+ CONSTRAINT [PK_EntryVersion] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX  = OFF, 
+		STATISTICS_NORECOMPUTE  = OFF, 
+		IGNORE_DUP_KEY = OFF, 
+		ALLOW_ROW_LOCKS  = ON, 
+		ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
