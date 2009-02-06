6527d745dca3d4c5a5e457e4ece96e58a7f244de
chsword_cp <SND\chsword_cp>
2009-02-06 08:03:19 +0000

    supernote abstraction

---
+++ CHSNS#1.X/CHSNS.Data.Abstractions/ICURDMediator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CHSNS.Data {
+    public interface ICURDMediator<T> {
+        void Create(T content);
+        void Update(T content);
+        void Remove(params long[] uid);
+        IQueryable<T> List(long? uid);
+    }
+}
--- CHSNS#1.X/Web/Controllers/VideoController.cs
+++ CHSNS#1.X/Web/Controllers/VideoController.cs
@@ -2,52 +2,34 @@ using System;
 using System.Web.Mvc;
 using CHSNS.Models;
 
-namespace CHSNS.Controllers
-{
-	
-	public class VideoController : BaseController
-	{
-		[LoginedFilter]
-		[AcceptVerbs(HttpVerbs.Get)]
-		public ActionResult Edit(){
-			Title = "提交视频";
-			return View();
-		}
-		[LoginedFilter]
-		[AcceptVerbs(HttpVerbs.Post)]
-		public ActionResult Edit(SuperNote v){
-			v.AddTime = DateTime.Now;
-			var m = new Media(v.Url);
-			v.Title = v.Title ?? m.Title;
-			v.Faceurl = m.Pic;
-			v.UserID = CHUser.UserID;
-			DBExt.DB.SuperNote.InsertOnSubmit(v);
-			DBExt.DB.SubmitChanges();
-			Message = "提交成功";
-			return Edit();
-		}
+namespace CHSNS.Controllers {
 
-		public ActionResult List() {
-			//SuperNoteList snl = new SuperNoteList();
-			//snl.Everypage = 10;
-			//snl.Nowpage = 1;
-			//snl.Template = "supernotepage";
-			//snl.Userid = this.QueryLong("userid") == 0
-			//    ? CHSNSUser.Current.UserID : this.QueryLong("userid");
-			//ViewData["page"] = snl.CreateUserSuperNote().ToString();
-			return View();
-		}
-		public ActionResult Random() {
-			//SuperNoteList snl = new SuperNoteList();
-			//snl.Everypage = 10;
-			//snl.Nowpage = 1;
-			//snl.Template = "SuperNoteRandomPage";
-
-			//snl.Type = 1;
-			//snl.Userid = CHUser.UserID;
-
-			//ViewData["page"] = snl.CreateUserSuperNote().ToString();
-			return View();
-		}
-	}
+    public class VideoController : BaseController {
+        [LoginedFilter]
+        [AcceptVerbs(HttpVerbs.Get)]
+        public ActionResult Edit() {
+            Title = "提交";
+            return View();
+        }
+        [LoginedFilter]
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Edit(SuperNote v) {
+            DBExt.Video.Create(v);
+            Message = "提交成功";
+            return Edit();
+        }
+        /// <summary>
+        /// 我的列表
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult List(long? uid, int? p) {
+            InitPage(ref p);
+            ViewData["list"] = DBExt.Video.List(uid).Pager(p.Value, 10);
+            return View();
+        }
+        public ActionResult Del(long[] uid) {
+            DBExt.Video.Remove(uid);
+            return View();
+        }
+    }
 }
