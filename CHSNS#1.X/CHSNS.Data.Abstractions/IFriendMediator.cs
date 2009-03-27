3833d234d8431f1837d3e2ffe6e2f78a5419857a
chsword_cp <SND\chsword_cp>
2009-03-27 08:59:21 +0000

    Friend & friend request decoupling

--- CHSNS#1.X/CHSNS.Data.Abstractions/IFriendMediator.cs
+++ CHSNS#1.X/CHSNS.Data.Abstractions/IFriendMediator.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using CHSNS.Model;
 using CHSNS.Models;
 
@@ -10,12 +9,15 @@ namespace CHSNS.Service
         bool Add(long FromID, long ToID);
         bool Agree(long OperaterID, long ToID);
         bool Delete(long FromID, long ToID);
-        PagedList<UserItemPas> GetFriends(long uid, int p, int ep);
-        List<long> GetFriendsID(long userid);
-        IQueryable<UserItemPas> GetRandoms();
-        IQueryable<UserItemPas> GetRequests(long userid);
         bool Ignore(long FromID, long operaterID);
         bool IgnoreAll(long UserID);
+
+        PagedList<UserItemPas> GetFriends(long uid, int p);
+        IList<long> GetFriendsID(long userid);
+        IList<UserItemPas> GetRandoms(int n);
+        PagedList<UserItemPas> GetRequests(long userid,int p);
+
         Profile UserFriendInfo(long userid);
+
 	}
 }
---
+++ CHSNS#1.X/CHSNS.Models/Config/EveryPageConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace CHSNS.Config {
+    public class EveryPageConfig {
+        public int NoteComment { get; set; }
+        public int Friend { get; set; }
+        public int FriendRequest { get; set; }
+    }
+}
