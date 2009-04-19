ae6579e728bca2d45c70cc70fed2acafed52bc81
chsword_cp <SND\chsword_cp>
2009-04-19 15:40:06 +0000

    un run,refact account

--- CHSNS#1.X/CHSNS.Data.Abstractions/IAccountOperator.cs
+++ CHSNS#1.X/CHSNS.Data.Abstractions/IAccountOperator.cs
@@ -3,8 +3,8 @@ namespace CHSNS.Operator {
 	public interface IAccountOperator {
 		bool Create(AccountPas account, string name);
 		bool IsUsernameCanUse(string username);
-		int Login(string Username, string Password, bool IsAutoLogin, bool IsPasswordMd5);
-		void Logout();
+		int Login(string userName, string password, bool autoLogin, bool isPasswordMd5,IContext context);
+		void Logout(IContext context);
         /// <summary>
         /// 初始化创建者
         /// </summary>
