4ebc6d0030a22a5ffffe49675a16460086de500d
chsword_cp <SND\chsword_cp>
2009-04-24 09:36:17 +0000

    unrun save stream to file

--- CHSNS#1.X/CHSNS.Common/LocalImplement/LocalStoreFile.cs
+++ CHSNS#1.X/CHSNS.Common/LocalImplement/LocalStoreFile.cs
@@ -18,11 +18,27 @@ namespace CHSNS.LocalImplement{
            
         }
 
+        public void Save(Stream inputStream, string filename)
+        {
+            var s = inputStream; //这是你获得的流  
+            var buffer = new byte[s.Length];
+            s.Read(buffer, 0, buffer.Length); //将流的内容读到缓冲区  
+            using (var fs = new FileStream(
+                Context.HttpContext.Server.MapPath(filename),
+                FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                fs.Write(buffer, 0, buffer.Length);
+                fs.Flush();
+            }
+        }
+
         public void WriteLine(string path, string text){
             using (var sw = new StreamWriter(path, true)) {
                 sw.WriteLine(text);
+                
             }
         }
+
 
         public void Delete(string path){
             var pathserver = Context.HttpContext.Server.MapPath(path);
