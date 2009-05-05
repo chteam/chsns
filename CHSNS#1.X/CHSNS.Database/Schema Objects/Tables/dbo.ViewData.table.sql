ee77c0fecab415b53d65604f24237b70f59da919
chsword_cp <SND\chsword_cp>
2009-05-05 03:53:48 +0000

    refact db

--- CHSNS#1.X/CHSNS.Database/Schema Objects/Tables/dbo.ViewData.table.sql
+++ CHSNS#1.X/CHSNS.Database/Schema Objects/Tables/dbo.ViewData.table.sql
@@ -3,7 +3,7 @@
 [Id] [bigint] NOT NULL IdENTITY(1, 1),
 [OwnerId] [bigint] NOT NULL,
 [ViewerId] [bigint] NOT NULL,
-[IpandComputer] [nvarchar] (50) NULL,
+[IPandComputer] [nvarchar] (50) NULL,
 [ViewPageUrl] [nvarchar] (255) NULL,
 [LastUrl] [nvarchar] (255) NULL,
 [ViewTime] [datetime] NOT NULL,
