f46b829461f94754c0cc481386d04a91a10c5162
chsword_cp <SND\chsword_cp>
2009-05-20 02:30:02 +0000

    add ibatisnet

--- CHSNS/CHSNS.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CHSNS.Web.GlobalApplication" Language="C#" %>
--- CHSNS/CHSNS.Web/Js/test.js
+++
@@ -1,3 +0,0 @@
-﻿/// <reference path="jquery-1.2.3-intellisense.js" />
-//添加了上一行在这里就可以使用jquery的智能提示了，要注意文件路径
-$('a').find('.red')
