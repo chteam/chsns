d02a8eab55bd9e644d61ca72fb1b90a398f3a698
chsword_cp <SND\chsword_cp>
2010-05-09 06:06:28 +0000

    Create MythBusters

---
+++ MythBusters/MythBusters/MythBusters/Controllers/BaseController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using XiaoNei;
+
+namespace MythBusters.Controllers
+{
+	[SessionKeyFilter]
+    public class BaseController : Controller,IXiaoNeiHandler
+    {
+    	public XiaoNeiApi Api { get; set; }
+    	public bool IsDebug { get; set; }
+    	public string UserID { get; private set; }
+		public XiaoNeiApi GetApi()
+		{
+            return Api = new XiaoNeiApi("c7f9c729cd74485897f033609083ba75",
+                "26e76666962147549ee365330b692b96",
+						 Request.QueryString["xn_sig_session_key"], this);
+		}
+
+    }
+}
