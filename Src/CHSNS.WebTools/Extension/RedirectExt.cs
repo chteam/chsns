dd87eaff451a1298e4486c5b495c03c33c93361c
Zou Jian <chsword@126.com>
2017-04-08 16:21:40 +0800

    add web

--- Src/CHSNS.WebTools/Extension/RedirectExt.cs
+++ Src/CHSNS.WebTools/Extension/RedirectExt.cs
@@ -6,7 +6,7 @@ namespace CHSNS
 		/// <summary>
 		/// 重定向到上一个Action. 即 header 的 "HTTP_REFERER"  (<c>Context.UrlReferrer</c>).
 		/// </summary>
-		static public RedirectResult RedirectToReferrer(this Controller controller) {
+		public static RedirectResult RedirectToReferrer(this Controller controller) {
 			return new RedirectResult(
 				""//controller.Request.ServerVariables["HTTP_REFERER"]
 				);
